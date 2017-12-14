762a2f5f8d1951c89b19d8ebf04191ec34f5d2f8
oyushche <uhmateush@gmail.com>
2017-12-14 03:54:57 -0500

    init project

---
+++ src/com/company/Bird.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Bird
+{
+    private String name;
+
+    private int age;
+
+    public String getName()
+    {
+        return name +  "duscks";
+    }
+
+    public void setName(String name)
+    {
+        System.out.println(name);
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("!!!!! :)");
+
+        System.out.println(":)");
+        System.out.println("bird");
+
+        System.out.println("bird2232");
+    }
+}
